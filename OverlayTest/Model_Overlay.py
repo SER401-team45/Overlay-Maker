0f25575ad1579ee4f7e913433119f3b2551eb8d4
DESKTOP-17G0L6T\Erich <ejkoch1@asu.edu>
2021-02-09 20:24:00 -0700

    Introduce basic integration of overlay with detection model

---
+++ OverlayTest/Model_Overlay.py
@@ -0,0 +1,165 @@
+"""
+Script for running YOLO model with overlay detection
+"""
+import cv2
+import numpy as np
+import json
+from shapely.geometry import Polygon
+
+net = cv2.dnn.readNet('yolov3_2_5_2021.weights', 'yolov3_testing.cfg')
+
+classes = []
+with open('coco_custom.names', 'r') as f:
+    classes = f.read().splitlines()
+
+cap = cv2.VideoCapture('Emergency 2 (Aaron).mov')  # name of video file here
+#  cap = cv2.VideoCapture(0)
+
+
+overlay_file = 'emergency_1_aaron_image_overlay.json'
+#  overlay_file = 'webcam_test_overlay.json'
+
+#  Dictionary of Region objects
+Regions = {}
+
+#  Font for overlay text
+font = cv2.FONT_HERSHEY_PLAIN
+
+#  Confidence threshold for model detection
+threshold = 0.1
+
+# "True" = Using Region Overlap, "False" = Point Containment
+overlap_type = True
+
+
+#  Class for Region data
+class Region:
+    def __init__(self, name_):
+        self.name = name_
+        self.point_list = []
+        self.numpy_point_list = None
+        self.intersected = False
+
+    def add_point(self, x_, y_):
+        self.point_list.append((x_, y_))
+
+
+#  Populating regions with data from JSON file
+with open(overlay_file) as json_file:
+    data = json.load(json_file)
+    for reg_json in data["data"]:
+        reg = Region(reg_json["name"])
+        for point_json in reg_json["points"]:
+            reg.add_point(float(point_json["x"]), float(point_json["y"]))
+        Regions[reg.name] = reg
+
+_, img = cap.read()
+height, width, _ = img.shape
+
+
+#  Scaling point data in Regions to fit image size
+for region in Regions:
+    for i in range(len(Regions[region].point_list)):
+        point = Regions[region].point_list[i]
+        p = list(point)
+        p[0] = p[0]*width
+        p[1] = p[1]*height
+        Regions[region].point_list[i] = tuple(p)
+    points = np.array(Regions[region].point_list)
+    pts = np.array(points, np.int32)
+    pts = pts.reshape((-1, 1, 2))
+    Regions[region].numpy_point_list = pts
+
+
+while True:
+    _, img = cap.read()
+    height, width, _ = img.shape
+    blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+    net.setInput(blob)
+    output_layers_names = net.getUnconnectedOutLayersNames()
+    layerOutputs = net.forward(output_layers_names)
+
+    boxes = []
+    confidences = []
+    class_ids = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > threshold:
+                center_x = int(detection[0]*width)
+                center_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
+
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
+
+                boxes.append([x, y, w, h])
+                confidences.append((float(confidence)))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, threshold, 0.4)
+
+    temp_boxes = []
+
+    detected_regions = []
+
+    if len(indexes) > 0:
+        for i in indexes.flatten():
+            temp_boxes.append(boxes[i])
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = str(round(confidences[i], 2))
+            color = (0, 255 * confidences[i] / (1 - threshold) - 255 * threshold / (1 - threshold),
+                     -255 * confidences[i] / (1 - threshold) + 255 / (1 - threshold))
+            cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
+            cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255, 255, 255), 2)
+
+            #  For each detected hand, loops through every region to check for overlap
+            for region in Regions:
+                p = Polygon(Regions[region].point_list)
+                q = Polygon([(x, y), (x+w, y), (x, y+h), (x+w, y+h)])
+                if overlap_type:
+                    if p.intersects(q):
+                        Regions[region].intersected = True
+                        detected_regions.append(region)
+                else:
+                    if p.contains(q.centroid):
+                        Regions[region].intersected = True
+                        detected_regions.append(region)
+
+    #  Drawing polygon for each region
+    for region in Regions:
+        if Regions[region].intersected:
+            color = (0, 0, 255)
+        else:
+            color = (0, 255, 0)
+        cv2.polylines(img, [Regions[region].numpy_point_list], True, color, 2)
+        Regions[region].intersected = False
+
+    img = cv2.resize(img, (int(width/2), int(height/2)))
+
+    sub_img = img[0:int((len(detected_regions) + 1)*22), 0:150]
+    black_rect = np.ones(sub_img.shape, dtype=np.uint8) * 0
+    res = cv2.addWeighted(sub_img, 0.25, black_rect, 0.5, 1.0)
+    img[0:int((len(detected_regions) + 1)*22), 0:150] = res
+
+    text_color = (255, 255, 255)
+    text_thickness = 2
+    cv2.putText(img, "Active: ", (0, 20), font, 1.3, text_color, text_thickness)
+    y_pos = 40
+    for region in detected_regions:
+        cv2.putText(img, region, (0, y_pos), font, 1.3, text_color, text_thickness)
+        y_pos += 20
+
+    cv2.imshow('Image', img)
+    key = cv2.waitKey(1)
+
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Overlay-Maker
+
+Python based GUI for creating polygonal regions for video overlay.
+Saves overlay to JSON format.
+
+
+Use:
+- Image must be loaded before overlay construction.
+- Existing overlay files can be loaded and edited.
+- Regions with non-empty, unique names may be added to the overlay.
+- The last point added to a region always connects to the first point.
+- The last point of a region may be deleted
+- Entire regions may be deleted
+- Existing regions may be selected from the region list.
+- Completed overlays may be saved to JSON files.
+
+Hot-Key shortcuts are shown under the "Shortcuts" dropdown
+
+A grid overlay under "Tools" may be displayed to assist alignment.
+
+
+Running this script requires installation of Tkinter and PIL
+
+pip install Tkinter
+pip install PIL
+
+
+
+# Model_Overlay
+
+Model_Overlay.py requires libraries Numpy, cv2, and shapely.
+
+	pip install Numpy
+	pip install cv2
+	pip install shapely
+
+Note:
+shapely installed for me, but failed at runtime. The following commands resolved my issue.
+
+	conda config --add channels conda-forge
+	conda install shapely
